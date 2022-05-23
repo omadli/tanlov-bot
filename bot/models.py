ddbbca06bac7f206a7acf72d2918a8945524cf1e
Omadli Yigit <omadliyigit17@gmail.com>
2022-05-23 21:53:20 +0500

    Bug fix

--- bot/models.py
+++ bot/models.py
@@ -9,7 +9,7 @@ class TgUser(models.Model):
 class Contestant(models.Model):
     tg_user = models.ForeignKey(TgUser, on_delete=models.CASCADE, help_text="Telegram user", unique=True)
     fish = models.CharField(max_length=200, help_text="Familiya Ism Sharif")
-    birth_date = models.CharField(max_length=50. help_text="Tug'ilgan kun: dd.mm.yyyy shaklda")
+    birth_date = models.CharField(max_length=50, help_text="Tug'ilgan kun: dd.mm.yyyy shaklda")
     adress = models.CharField(max_length=200, help_text="To'liq manzil: viloyat, mahalla, ko'cha, xonadon")
     tel = models.PositiveBigIntegerField(max_length=12, help_text="Telefon raqami: + siz")
     files = models.TextField(null=False, help_text="JSON formatdagi telegram medialari ro'yxati")
