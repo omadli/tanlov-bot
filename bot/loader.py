9202fdbcf0e6bd2132e43013ad0eb84bd69cad80
Omadli Yigit <omadliyigit17@gmail.com>
2022-05-23 22:03:55 +0500

    Loader edit

--- bot/loader.py
+++ bot/loader.py
@@ -1,6 +1,8 @@
 from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from src.settings import BOT_TOKEN
 
 bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
